6fe17a02e3a42641f47f636ad581dc3bacc02e03
Basir Qurbani <basir@Basir.local>
2020-09-04 15:03:31 -0600

    Updates

---
+++ precache-manifest.8d3417bc7db7aba3f3f74723840f5966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3683c8156d18778c337f0c5ad5fcdbb4",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "2538e134e85f9455c14c",
+    "url": "/covid19/static/css/main.83878636.chunk.css"
+  },
+  {
+    "revision": "a89a16df963cdf69713d",
+    "url": "/covid19/static/js/2.9f6fdc9e.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19/static/js/2.9f6fdc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2538e134e85f9455c14c",
+    "url": "/covid19/static/js/main.a7c3a577.chunk.js"
+  },
+  {
+    "revision": "636bdfdab0a46f674c1f",
+    "url": "/covid19/static/js/runtime-main.09a13a41.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
